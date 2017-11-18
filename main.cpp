5374c331febcaeb24a421cb5d4ec59b50abb21bf
Pavel Kukshtel <kukshtel.pavel@gmail.com>
2017-11-18 04:42:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,28 @@
-#include <cstdlib>
 #include <iostream>
-#include "SDL2/SDL.h"
 
-int main(int argc, char* args[]) {
+#include <SDL2/SDL_version.h>
 
-  SDL_Init(SDL_INIT_EVERYTHING);
+std::ostream& operator<<(std::ostream& out, const SDL_version& v) {
+  out << static_cast<int>(v.major) << '.';
+  out << static_cast<int>(v.minor) << '.';
+  out << static_cast<int>(v.patch);
+  return out;
+}
 
-  SDL_version compiled;
-  SDL_version linked;
+int main(int /*argc*/, char* /*argv*/ []) {
+  using namespace std;
+
+  SDL_version compiled{0, 0, 0};
+  SDL_version linked{0, 0, 0};
 
   SDL_VERSION(&compiled);
   SDL_GetVersion(&linked);
 
-  std::cout << "SDL version -"
-            << static_cast<int>(compiled.major) << '.'
-            << static_cast<int>(compiled.minor) << '.'
-            << static_cast<int>(compiled.patch) << std::endl;
-  std::cout << "linking SDL version -"
-            << static_cast<int>(linked.major) << '.'
-            << static_cast<int>(linked.minor) << '.'
-            << static_cast<int>(linked.patch) << std::endl;
+  cout << "compiled: " << compiled << '\n';
+  cout << "linked: " << linked << endl;
 
-  bool is_good = std::cout.good();
+  bool is_good = cout.good();
+
   int result = is_good ? EXIT_SUCCESS : EXIT_FAILURE;
-
   return result;
-
 }
